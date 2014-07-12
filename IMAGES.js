a16fd52c98802e1910ab32dffa3ba2fd55d94e13
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2014-07-12 18:20:46 +0200

    add example3 and index page

---
+++ IMAGES.js
@@ -0,0 +1,14 @@
+var IMAGES = [
+  "images/whtXWmDGTTuddi1ncK5v_IMG_0097.jpg",
+  "images/wdXqHcTwSTmLuKOGz92L_Landscape.jpg",
+  "images/yIdlmSvfSZCyGkCkLt0P_lucaslof_2.jpg",
+  "images/yvDPJ8ZSmSVob7pRxIvU_IMG_40322.jpg",
+  "images/mtNrf7oxS4uSxTzMBWfQ_DSC_0043.jpg",
+  "images/10294420724_0300cf921d_o.jpg",
+  "images/2wu8LKZQfahkG6HMeKf6_14546548685_aea346dd84_o.jpg",
+  "images/483LKAktTE2ZB1UIkNYY_3.jpg",
+  "images/BZVRjxwfRaoelGxNSGTA_P1070545.jpg",
+  "images/F9g2FiPUSyC7IybhyI2p_09.jpg",
+  "images/IKMsqwyaR6qN0kq48STw_annapurna.jpg",
+  "images/vsSIIQbXTHCO1jBsKm4Q_IMG_1779_edt.jpg"
+];
